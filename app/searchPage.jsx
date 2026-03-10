29aab9e9bcce258065dff355ca8141fe5fda13bd
Jinson <jinson@Jinsons-MacBook-Air.local>
2026-03-10 12:38:23 +0000

    connects searchbar to searchpage

--- app/searchPage.jsx
+++ app/searchPage.jsx
@@ -2,11 +2,13 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
 import NavigationBar from "./navigationBar";
+import SearchBar from "./searchBar";
 
 const SearchPage = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Search</Text>
+      <SearchBar />
       <NavigationBar />
     </View>
   );
