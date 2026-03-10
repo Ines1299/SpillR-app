fb16beb34dd901d9ea9310011014e54e0d184507
Ines Cadete <inesmcadete@hotmail.com>
2026-03-10 13:57:15 +0000

    Fixed the routing with tabs.screen

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,64 @@
+import { Tabs } from "expo-router";
+import { Image } from "react-native";
+
+import homeIcon from "../../assets/home.png";
+import searchIcon from "../../assets/search-normal.png";
+import userIcon from "../../assets/user.png";
+import notificationIcon from "../../assets/notification-status.png";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#484848",
+          height: 60,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <Image source={homeIcon} style={{ width: 24, height: 24 }} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="search"
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <Image source={searchIcon} style={{ width: 24, height: 24 }} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <Image
+              source={notificationIcon}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="user"
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <Image source={userIcon} style={{ width: 24, height: 24 }} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
