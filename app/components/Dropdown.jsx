b9f1f86c040a53b46f23fa10fae84d664dac203c
3llasious <quest4ella22@gmail.com>
2026-03-14 08:57:56 +0000

    made some styling chnages

--- app/components/Dropdown.jsx
+++ app/components/Dropdown.jsx
@@ -1,32 +1,25 @@
-import { getSeasonsAndEpisodesByShowName } from "../../utils/utilsFunctions";
-
 import { useState, useEffect } from "react";
 import { Image, View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import EpisodesList from "./EpisodesList";
 
-export default function Dropdown({ name }) {
-  const [seasons, setSeasons] = useState([]);
+export default function Dropdown({ name, seasons }) {
   const [selectedSeason, setSelectedSeason] = useState({});
+  const [latestSeason, setLatestSeason] = useState(null);
   const [visible, setVisible] = useState(false);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    async function loadSeasonsAndEpisodes() {
-      try {
-        setLoading(true);
-        const data = await getSeasonsAndEpisodesByShowName(name);
+    if (seasons.length > 0) {
+      let latestSeason = "";
+      latestSeason =
+        seasons[seasons.length - 1].episodes.length > 0
+          ? seasons[seasons.length - 1]
+          : seasons[seasons.length - 2];
 
-        setSeasons(data.seasons);
-        setSelectedSeason(data.seasons[0]);
-      } catch (error) {
-        console.log("Error loading seasons:", error);
-      } finally {
-        setLoading(false);
-      }
+      setSelectedSeason(latestSeason);
+      setLatestSeason(latestSeason);
     }
-
-    loadSeasonsAndEpisodes();
-  }, []);
+  }, [name]);
 
   const toggleDropdown = () => {
     setVisible(!visible);
@@ -43,7 +36,11 @@ export default function Dropdown({ name }) {
         <Text style={styles.buttonText}>
           {loading
             ? "Loading seasons..."
-            : `Season ${selectedSeason.season_number}`}
+            : selectedSeason.season_number > latestSeason?.season_number
+              ? `Upcoming Season ${selectedSeason.season_number}`
+              : selectedSeason.season_number === latestSeason?.season_number
+                ? `Latest Season`
+                : `Season ${selectedSeason.season_number}`}
         </Text>
         <Image
           style={styles.dropdownArrow}
@@ -59,7 +56,9 @@ export default function Dropdown({ name }) {
               style={styles.dropdownItem}
               onPress={() => handleSelectSeason(season)}
             >
-              <Text>Season {season.season_number}</Text>
+              <Text style={styles.dropdownItem}>
+                Season {season.season_number}
+              </Text>
             </TouchableOpacity>
           ))}
         </View>
@@ -72,14 +71,18 @@ export default function Dropdown({ name }) {
 
 const styles = StyleSheet.create({
   container: {
+    top: 50,
     width: "100%",
+    zIndex: 999,
+    backgroundColor: "#1B1B1B",
     marginLeft: 0,
   },
   button: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between",
-    backgroundColor: "#efefef",
+    justifyContent: "flex-start",
+    gap: 30,
+    backgroundColor: "#1B1B1B",
     height: 40,
     paddingHorizontal: 10,
     borderRadius: 6,
@@ -87,21 +90,31 @@ const styles = StyleSheet.create({
 
   buttonText: {
     fontSize: 14,
+    color: "#FFFFFF",
   },
 
   dropdownArrow: {
     width: 14,
     height: 14,
+    tintColor: "#FFFFFF",
+    resizeMode: "contain",
   },
 
   dropdown: {
-    backgroundColor: "#fff",
+    position: "absolute",
+    width: "30%",
+    top: 45, //button height
+    left: 0,
+    right: 0,
+    backgroundColor: "#1B1B1B",
     borderRadius: 6,
-    marginTop: 5,
     paddingVertical: 5,
+    zIndex: 1000,
+    elevation: 5,
   },
 
   dropdownItem: {
     padding: 8,
+    color: "#FFFFFF",
   },
 });
